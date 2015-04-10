f120e0cae1aa2a28382eb313eaad87fde89a296b
emannuelOC <emannuel.carvalho@outlook.com>
2015-04-10 18:32:14 -0300

    Initial Commit

---
+++ IMC_ES/src/imc3/IMC_Project.java
@@ -0,0 +1,24 @@
+
+package imc3;
+
+/**
+ *
+ * @author 41330056
+ */
+import imc3.Ex2;
+import javax.swing.JFrame;
+
+//Main - Nome da classe
+public class IMC_Project {
+
+    //Comando principal para execução do programa
+
+    public static void main(String[] args) {
+        //Criação de um objeto ex da classe Ex2
+        Ex2 ex = new Ex2();
+        //posição da tela ao abrir
+        ex.setLocation(400, 200);
+        //tornando a tela visível
+        ex.setVisible(true);
+    }
+}
---
+++ IMC_ES/src/imc_es/IMC_ES.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package imc_es;
+
+/**
+ *
+ * @author Emannuel
+ */
+public class IMC_ES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
